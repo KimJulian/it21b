638672be74ae9f156f30acff964df1c61f5e09f2
Kimjulian <mentopakimjulian@gmail.com>
2025-08-29 09:46:21 +0800

    Added subtraction, multiplication and division

---
+++ hello.js
@@ -0,0 +1,14 @@
+console.log ("hello im Kim");
+
+num1 = 4;
+num2 = 6;
+
+sum = num1 + num2;
+diff = num1 - num2;
+prod = num1 * num2;
+quo = num1 / num2;
+
+console.log (" The sum of " + num1 + " and " + num2 + " is " +sum);
+console.log (" The difference of " + num1 + " and " + num2 + " is " +diff);
+console.log (" The product of " + num1 + " and " + num2 + " is " +prod);
+console.log (" The quotient of " + num1 + " and " + num2 + " is " +quo);
