24c6720ccb5fcd26aa73cd809959b6ac2f46fd37
Kimjulian <mentopakimjulian@gmail.com>
2025-09-16 08:20:50 +0800

    Added Warrior Class a inherited from Hero Class

--- hero.js
+++ hero.js
@@ -19,13 +19,14 @@ class Hero{
         console.log("Name: "+ this.name);
         console.log("Health: "+ this.#health);
         console.log("Name:"+ this.attack);
+        console.log("----------------------");
     }
     
     
 }
 
-const hero1 = new hero ("Thorin",100,10);
-const hero2 = new hero ("Gandalf",100,8);
+const hero1 = new Hero("Thorin",100,10);
+const hero2 = new Hero("Gandalf",100,8);
 
 hero1.showStats();
 hero2.showStats();
