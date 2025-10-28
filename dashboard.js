1e563aac9a5a6ea366fbd743e54193b56e63a1e0
Kimjulian <mentopakimjulian@gmail.com>
2025-10-28 13:38:06 +0800

    added class RadarChart

--- dashboard.js
+++ dashboard.js
@@ -60,3 +60,63 @@ document.addEventListener("DOMContentLoaded", () => {
     chart.init();
 });
 
+class RadarChart {
+
+        constructor(canvasId, dataUrl) {
+            this.canvasId = canvasId;
+            this.dataUrl = dataUrl;
+            this.chart = null;
+        }
+        renderChart(data) {
+            const ctx = document.getElementById(this.canvasId).getContext("2d");
+
+            this.chart = new Chart(ctx, {
+                type: "radar",
+                data: {
+                    labels: data.labels,
+                    datasets: data.datasets
+                },
+                option: {
+                    responsive: true,
+                    plugins: {
+                        legend: {
+                            position: "top"
+                        },
+                        title: {
+                            display: true,
+                            text: "Radar Chart"
+                        }
+                    },
+                    scales: {
+                        r: {
+                            angleLines: { display: true },
+                            suggestedMin: 0,
+                            suggestedMax: 100,
+                        }
+                    }
+                }
+            });
+        }
+        async fetchData() {
+            try {
+                const response = await fetch(this.dataUrl);
+                if (!response.ok) throw new Error(`failed to load data: ${response.statusText}`);
+
+                const data = await response.json();
+                return data;
+            } catch (error) {
+                console.error("error fetching data: ", error);
+                return null;
+            }
+        }
+
+        async init(){
+            const data = await this.fetchdata();
+            if(data) this.renderChart(data);
+        }
+    }
+    document.addEventListener("DOMContentLoaded", ()=>{
+        const chart = new radarChart("radarChart","radarData.json");
+        chart.init();
+    });
+    
